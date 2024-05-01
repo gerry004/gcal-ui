35007206569a99e7ea09e366a451fb6d2918ecb3
Gerry Yang <gerry04y@gmail.com>
2024-05-01 17:31:46 +0100

    Tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 function App() {
+  const signIn = async () => {
+    console.log('Sign In')
+  }
+
   return (
     <div className='text-center w-screen h-screen flex justify-center items-center'>
       Hello World
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={signIn}>Sign In</button>
     </div>
   );
 }
